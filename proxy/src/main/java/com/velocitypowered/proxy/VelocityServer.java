477f3e3360b031a1b39a92b9e62d7de8f7d5806d
r00tb33rman <rootb33rman@gmail.com>
2024-09-16 15:08:11 -0400

    Reorganize small set of code

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -227,8 +227,8 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
   private VelocityPluginContainer createVirtualPlugin() {
     ProxyVersion version = getVersion();
     PluginDescription description = new VelocityPluginDescription(
-            "velocity", version.getName(), version.getVersion(), "The Velocity proxy",
-            VELOCITY_URL, ImmutableList.of(version.getVendor()), Collections.emptyList(), null);
+        "velocity", version.getName(), version.getVersion(), "The Velocity proxy",
+        VELOCITY_URL, ImmutableList.of(version.getVendor()), Collections.emptyList(), null);
     VelocityPluginContainer container = new VelocityPluginContainer(description);
     container.setInstance(VelocityVirtualPlugin.INSTANCE);
     return container;
